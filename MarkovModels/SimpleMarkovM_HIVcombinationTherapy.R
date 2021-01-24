3b15253131d71f780ceb8adc22250f52a7f37a9b
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2021-01-24 14:51:25 -0800

    Create SimpleMarkovM_HIVcombinationTherapy.R
    This is a sample of a Simple Markov Model (Homogeneous)

---
+++ MarkovModels/SimpleMarkovM_HIVcombinationTherapy.R
@@ -0,0 +1,12 @@
+# In this model we will present a Simple Markov Model to study the 
+# cost effectiveness of 2 different drugs to threat HIV
+# (lamivudine/zidovudine combination Therapy) - further described in
+# the book Decision Modeling for Health Economic Evaluation by Chancellor, 1997
+
+# This is simpler extract of the analysis from the book.
+
+
+# GOAL:
+# The model aims to compare costs and utilities of two treatment strategies,
+# monotherapy and combined therapy.
+
